a8a4d5fd580fc4b8025d498aef746f510c762b15
kazibweyassin <kazibweusama@gmail.com>
2024-03-10 10:30:57 -0700

    added

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    matrix = []
+    if n <= 0:
+        return matrix
+    matrix = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(matrix[i -1 ]) - 1):
+            current  = matrix[i -1]
+            temp.append(matrix[i - 1][j] + matrix[i - 1][j + 1])
+        temp.append(1)
+        matrix.append(temp)
+    return matrix
