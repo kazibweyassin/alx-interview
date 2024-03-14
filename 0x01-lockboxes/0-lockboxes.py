62f425bff0a2a53f463d787bd66cbfa47382b62d
kazibweyassin <kazibweusama@gmail.com>
2024-03-14 23:36:06 +0300

    some boxes are unlocked

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+def canUnlockAll(boxes):
+    """Determine if all the boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists representing the lockboxes and their corresponding keys.
+
+    Returns:
+        bool: True if all the boxes can be opened, False otherwise.
+    """
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    return len(keys) == len(boxes)
